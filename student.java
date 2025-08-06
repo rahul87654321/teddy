ae9f80c8ed3f75528daf76e1b9c7f3337a2ba5ec
rahul87654321 <anithasunar5@gmail.com>
2025-08-06 15:49:38 +0530

    Update student.java

--- student.java
+++ student.java
@@ -1,6 +1,7 @@
 import java.io.*;
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, my name is rahul");
     }
 }
+
